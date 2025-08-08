6c65c496b61e58c09902ed03a78860ae32234a17
Rune <sungjong5k@naver.com>
2025-08-09 00:53:20 +0900

    docs: remove custom parser preset from commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,23 +1,16 @@
 const config = {
-  parserPreset: {
-    parserOpts: {
-      // 커스텀 파싱 패턴: [type]: description
-      headerPattern: /^\[(\w+)\]: (.+)$/,
-      headerCorrespondence: ['type', 'subject'],
-    },
-  },
   rules: {
     'type-enum': [
       2,
       'always',
       [
-        'feat', // 기능
-        'ui', // 화면 디자인
-        'refactor', // 기능 변경은 없이, 코드만 수정
-        'fix', // 기능 문제 수정, 버그나 오류 수정
-        'chore', // 주석, 들여쓰기
-        'docs', // 문서작업, Readme, CI(workflow)
-        'init', // 프로젝트 기초 세팅
+        'feat', // A new feature
+        'ui', // UI or style changes
+        'refactor', // A code change that neither fixes a bug nor adds a feature
+        'fix', // A bug fix
+        'chore', // Changes to the build process or auxiliary tools and libraries
+        'docs', // Documentation only changes (e.g., README, CI workflow)
+        'init', // Initial project setup
       ],
     ],
     'type-case': [2, 'always', 'lower-case'],
