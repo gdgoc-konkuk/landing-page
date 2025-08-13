9bc533ae950554cc3b4ce983f84573717bbc7b44
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-08-13 18:19:48 +0900

    refactor: change color constants to config colors

--- config/activity.config.ts
+++ config/activity.config.ts
@@ -1,20 +1,16 @@
-﻿// tailwind.config.ts에 정의한 색상 값을 변수로 관리
-export const G_COLORS = {
-  BLUE: '#4285f4',
-  RED: '#ea4335',
-  YELLOW: '#f9ab00',
-  GREEN: '#34a853',
-  GRAY: '#f1f3f4',
-};
+﻿import resolveConfig from 'tailwindcss/resolveConfig';
+import tailwindConfig from '@/tailwind.config'
+
+const fullConfig = resolveConfig(tailwindConfig);
 
 export const ACTIVITY_DATA = [
   {
     title: 'Study',
     colors: {
-      topLeft: G_COLORS.GRAY,
-      topRight: G_COLORS.GRAY,
-      bottomLeft: G_COLORS.GRAY,
-      bottomRight: G_COLORS.GRAY,
+      topLeft: fullConfig.theme.colors.google.gray,
+      topRight: fullConfig.theme.colors.google.gray,
+      bottomLeft: fullConfig.theme.colors.google.gray,
+      bottomRight: fullConfig.theme.colors.google.gray,
     },
     description:
       '관심 기술을 <br/>함께 학습하며 <br/>지식을 나누는 <br/>소규모 모임',
@@ -23,10 +19,10 @@ export const ACTIVITY_DATA = [
   {
     title: 'Tech Talk',
     colors: {
-      topLeft: G_COLORS.GRAY,
-      topRight: G_COLORS.GRAY,
-      bottomLeft: G_COLORS.RED,
-      bottomRight: G_COLORS.GRAY,
+      topLeft: fullConfig.theme.colors.google.gray,
+      topRight: fullConfig.theme.colors.google.gray,
+      bottomLeft: fullConfig.theme.colors.google.red,
+      bottomRight: fullConfig.theme.colors.google.gray,
     },
     description:
       '개발자들이 <br/>인사이트와 경험을 <br/>공유하고 발표하는 <br/>기술 중심 세미나',
@@ -35,10 +31,10 @@ export const ACTIVITY_DATA = [
   {
     title: 'Hands-on',
     colors: {
-      topLeft: G_COLORS.BLUE,
-      topRight: G_COLORS.GRAY,
-      bottomLeft: G_COLORS.RED,
-      bottomRight: G_COLORS.GRAY,
+      topLeft: fullConfig.theme.colors.google.blue,
+      topRight: fullConfig.theme.colors.google.gray,
+      bottomLeft: fullConfig.theme.colors.google.red,
+      bottomRight: fullConfig.theme.colors.google.gray,
     },
     description: '실습 중심 워크숍으로 <br/>직접 체험해보는 세미나',
     backgroundImage: '/images/activity/activity-hands-on.jpg',
@@ -46,10 +42,10 @@ export const ACTIVITY_DATA = [
   {
     title: 'Networking',
     colors: {
-      topLeft: G_COLORS.BLUE,
-      topRight: G_COLORS.GRAY,
-      bottomLeft: G_COLORS.RED,
-      bottomRight: G_COLORS.GREEN,
+      topLeft: fullConfig.theme.colors.google.blue,
+      topRight: fullConfig.theme.colors.google.gray,
+      bottomLeft: fullConfig.theme.colors.google.red,
+      bottomRight: fullConfig.theme.colors.google.green,
     },
     description:
       '다양한 배경의 <br/>참가자들과 교류하며 <br/>커뮤니티를 확장하는 시간',
@@ -58,10 +54,10 @@ export const ACTIVITY_DATA = [
   {
     title: 'Community',
     colors: {
-      topLeft: G_COLORS.BLUE,
-      topRight: G_COLORS.YELLOW,
-      bottomLeft: G_COLORS.RED,
-      bottomRight: G_COLORS.GREEN,
+      topLeft: fullConfig.theme.colors.google.blue,
+      topRight: fullConfig.theme.colors.google.yellow,
+      bottomLeft: fullConfig.theme.colors.google.red,
+      bottomRight: fullConfig.theme.colors.google.green,
     },
     description:
       '팀워크와 재미를 <br/>동시에 느낄 수 있는 <br/>커뮤니티 중심 이벤트',
