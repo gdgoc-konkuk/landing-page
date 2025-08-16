9a42fb35a8e0af6ca6d6f62e8d771e5820b7bf91
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-08-16 22:02:10 +0900

    ui: add projectcard component

---
+++ app/_component/solution-challenge/ProjectCard.tsx
@@ -0,0 +1,35 @@
+﻿import Image from 'next/image';
+
+interface ProjectCardProps {
+  imageUrl: string;
+  title: string;
+  description: string;
+};
+
+export default function ProjectCard({
+  imageUrl,
+  title,
+  description,
+}: ProjectCardProps) {
+  return (
+    <div className="w-full mx-auto bg-gray-100 rounded-2xl shadow-md overflow-hidden">
+      <div className="relative w-full h-32 md:h-48">
+        <Image
+          src={imageUrl}
+          alt={`${title} project image`}
+          fill
+          style={{ objectFit: 'cover' }}
+          sizes="(max-width: 768px) 33vw, 192px"
+        />
+      </div>
+      <div className="p-2 sm:p-3 md:p-5">
+        <h3 className="text-sm md:text-xl font-semibold text-black tracking-tight truncate">
+          {title}
+        </h3>
+        <p className="mt-1 text-xs md:text-base text-gray-600 tracking-tight line-clamp-2">
+          {description}
+        </p>
+      </div>
+    </div>
+  );
+}
