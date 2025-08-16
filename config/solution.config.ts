7677e415c3481d6bfe37fb5a71c62b8e8bcae650
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-08-16 22:00:09 +0900

    ui: add solution challenge dummy constants and types

---
+++ config/solution.config.ts
@@ -0,0 +1,37 @@
+﻿export const solutionChallengeData: solutionChallengeData[] = [
+  {
+    rank: 2,
+    rankText: 'TOP 100',
+    isTopThree: false,
+    imageUrl: '/images/solution/pathpal.webp',
+    title: 'PathPal',
+    description:
+      'PathPal은 시각장애인의 안전하고 독립적인 보행을 보장하며 그들에게 사회적 편의를 제공합니다.',
+  },
+  {
+    rank: 1,
+    rankText: 'TOP 3',
+    isTopThree: true,
+    imageUrl: '/images/solution/pathpal.webp',
+    title: 'PathPal',
+    description:
+      'PathPal은 시각장애인의 안전하고 독립적인 보행을 보장하며 그들에게 사회적 편의를 제공합니다.',
+  },
+  {
+    rank: 3,
+    rankText: 'TOP 100',
+    isTopThree: false,
+    imageUrl: '/images/solution/pathpal.webp',
+    title: 'PathPal',
+    description:
+      'PathPal은 시각장애인의 안전하고 독립적인 보행을 보장하며 그들에게 사회적 편의를 제공합니다.',
+  },
+];
+
+export const solutionChallengeImages = [
+  '/images/solution/pathpal.webp',
+  '/images/solution/pathpal.webp',
+  '/images/solution/pathpal.webp',
+  '/images/solution/pathpal.webp',
+  '/images/solution/pathpal.webp',
+];
