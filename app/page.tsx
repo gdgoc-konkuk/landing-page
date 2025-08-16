9a471d2fb6f91f091cf1015b3bd095b6ea39e23a
Rune <sungjong5k@naver.com>
2025-08-17 02:25:50 +0900

    feat: add Kprintf component to display event sections on the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import ActivitySection from '@/app/_component/activity/ActivitySection';
 import SolutionChallengeSection from '@/app/_component/solution-challenge/SolutionChallengeSection';
 import Apply from './_component/apply/Apply';
+import Kprintf from './_component/kprintf/Kprintf';
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <ActivitySection />
       <SolutionChallengeSection />
+      <Kprintf />
       <Apply />
     </div>
   );
