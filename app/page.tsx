3ef11db7ff0b8e114a93e56a37f40f34a3b74573
Rune <sungjong5k@naver.com>
2025-08-17 16:38:14 +0900

    fix: remove duplicate ActivitySection from Home component

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
     <div className="flex flex-col">
       <ActivitySection />
       <SolutionChallengeSection />
-      <ActivitySection />
       <Apply />
     </div>
   );
