ea67614f12289e2b113c7722745aa7a4669569f2
Bumshik Park <peter012677@naver.com>
2025-08-21 23:08:48 +0900

    fix: About Section changed sentences

--- app/_component/about/AboutSection.tsx
+++ app/_component/about/AboutSection.tsx
@@ -10,9 +10,9 @@ const AboutSection: React.FC = () => {
           GDGoC Konkuk
         </h1>
         <p className="text-google-Contents text-sm md:text-lg font-robotomono font-regular mt-4 leading-relaxed break-keep">
-          GDGoC Konkuk에서 경험할 수 있는 활동을 소개합니다!
+          GDGoC Konkuk는 협업, 공유, 도전의 기회를 제공합니다!
           <br />
-          다양한 활동을 통해 함께 성장할 수 있어요.
+          함께 성장하는 커뮤니티를 지향합니다.
         </p>
       </div>
       <div className="flex flex-col gap-10 md:flex-row justify-center items-center w-full">
