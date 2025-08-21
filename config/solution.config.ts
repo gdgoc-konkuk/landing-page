cf6f4d92c9eb050a1b2553136f06c7eba129757f
Rune <sungjong5k@naver.com>
2025-08-21 23:43:58 +0900

    fix: update image URLs and descriptions in solution config

--- config/solution.config.ts
+++ config/solution.config.ts
@@ -5,33 +5,22 @@
     isTopThree: false,
     imageUrl: '/images/solution/pathpal.webp',
     title: 'PathPal',
-    description:
-      '2024년 Global TOP 100에 선정되었습니다.',
+    description: '2024년 Global TOP 100에 선정되었습니다.',
   },
   {
     rank: 1,
     rankText: 'TOP 3',
     isTopThree: true,
-    imageUrl: '/images/solution/pathpal.webp',
+    imageUrl: '/images/solution/attempo.webp',
     title: 'Atempo',
-    description:
-      '2025년 Most Social Impact Award에 선정되었습니다.',
+    description: '2025년 Most Social Impact Award에 선정되었습니다.',
   },
   {
     rank: 3,
     rankText: 'TOP 100',
     isTopThree: false,
-    imageUrl: '/images/solution/pathpal.webp',
+    imageUrl: '/images/solution/glowalarm.webp',
     title: 'Glow-Alarm',
-    description:
-      '2024년 Global TOP 100에 선정되었습니다',
+    description: '2024년 Global TOP 100에 선정되었습니다',
   },
-];
-
-export const solutionChallengeImages = [
-  '/images/solution/pathpal.webp',
-  '/images/solution/pathpal.webp',
-  '/images/solution/pathpal.webp',
-  '/images/solution/pathpal.webp',
-  '/images/solution/pathpal.webp',
 ];
